33f4e0755332e5c1936fac03d99e6cfdc73e9a5a
tatumumi <93298315+tatumumi@users.noreply.github.com>
2022-10-26 09:56:19 -1000

    Update sampledataset.py

--- sampledataset.py
+++ sampledataset.py
@@ -44,73 +44,68 @@ check115 = False
 check150 = False
 check277 = False
 check444 = False
-
-for image in tqdm.tqdm(glob.glob('file-home-*F115W*_scale.fits')):
-        #subprocess.getoutput("/Applications/ds9 new_file_" + filter + ".fits")
-        #open each of the four images 
-        
-        print(image)
-        
-        #checks if 115W exists
-        if os.path.exists(image) == True:
-            check115 = True
-        else:
-            check115  = False
             
-        #checks if 150W exists
-        if os.path.exists(image.replace("F115W", "F150W")) == True:
-            check150 = True
-        else:
-            check150 = False
-            
-        #checks if 277W exists
-        if os.path.exists(image.replace("F115W", "F277W")) == True:
-            check277 = True
-        else:
-            check277 = False
-            
-        #checks if 444W exists
-        if os.path.exists(image.replace("F115W", "F444W")) == True:
-            check444 = True
-        else:
-            check444 = False
+all_f115w_images = glob.glob('file-home-*F115W*_scale.fits’)
+all_f150w_images = glob.glob('file-home-*F150W*_scale.fits’)
+all_f277w_images = glob.glob('file-home-*F277W*_scale.fits’)
+all_f444w_images = glob.glob('file-home-*F444W*_scale.fits’)
+                             
+for image in tqdm.tqdm(all_f115w_images):
+    if all_f115w_imgages.count(image):
+        check115 = True
+    else:
+        check115 = False
+                             
+    if all_f150w_images.count(image.replace("F115W", "F150W”)):
+        check150 = True
+    else:
+        check150 = False
+      
+    if all_f277w_images.count(image.replace("F115W", "F277W”)):
+        check277 = True
+    else:
+        check277 = False      
+                                            
+    if all_f444w_images.count(image.replace("F115W", "F444W”)):
+        check444 = True
+    else:
+        check444 = False                                        
         
-        
-        if check115 & check150 & check277 & check444 == True:
+    if check115 & check150 & check277 & check444 == True:
         #for exists(image.replace(filt,"f150w"))
-            print("all checks")
-            all_data_for_SN = []
-            k = 0
-            while k < len(filt):
-                   print(k)
-                   img_name = image.replace("F115W", filt[k])
-                   print(img_name)
-                   h = fits.open(img_name)
-                   print(img_name)
-                   k += 1
-                   data = h[0].data
-                   length = len(data)
-                   header = h[0].header
-                   bound = 0
-                   i = h[0].header["SN_I"]
-                   j = h[0].header["SN_J"]
-                   #check if supernova was planted
-                   if h[0].header["PLACED"] == 'yes':
-                      supernova = 1
-                    #adds image with nova into array
-                   else:
-                          supernova = 0
-                            #add filter adjustments for crop
-                   bound = check_bound(i,j)
-                   if bound == 1: 
-                        all_data_for_SN = crop32(all_data_for_SN) 
-                   else:
-                        continue
-                   h.close()
+        print("all checks")
+        all_data_for_SN = []
+        k = 0
+        while k < len(filt):
+               print(k)
+               img_name = image.replace("F115W", filt[k])
+               print(img_name)
+               h = fits.open(img_name)
+               print(img_name)
+               k += 1
+               data = h[0].data
+               length = len(data)
+               header = h[0].header
+               bound = 0
+               i = h[0].header["SN_I"]
+               j = h[0].header["SN_J"]
+               #check if supernova was planted
+               if h[0].header["PLACED"] == 'yes':
+                  supernova = 1
+                #adds image with nova into array
+               else:
+                      supernova = 0
+                        #add filter adjustments for crop
+               bound = check_bound(i,j)
+               if bound == 1: 
+                    all_data_for_SN = crop32(all_data_for_SN) 
+               else:
+                    continue
+               h.close()
                 
-            if bound == 1:
-                all_images.append(all_data_for_SN)
-                has_sup.append(supernova)      
+        if bound == 1:
+            all_images.append(all_data_for_SN)
+            has_sup.append(supernova)      
         
 #dump all_SN into supernovae
 pickle.dump((all_images, has_sup),open("all_images.pickle", 'wb'))
